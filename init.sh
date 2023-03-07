e9d12c6155266f11fcc7bd2cbccdf06e241ff8a1
Khalturin <mrjokko@gmail.com>
2023-03-07 23:02:04 +0300

    make a config for ngingx

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Перед запуском скрипта:
+# git clone https://github.com/your_account/stepic_web_project.git /home/box/web
+# bash /home/box/web/init.sh
+
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -s /homebox/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
